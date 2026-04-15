83f8312915ba51decbdde20845b92bff66d11444
Prity Kumari <pritykumari.mca01@gmail.com>
2026-04-15 21:57:54 +0530

    final portfolio  polish

---
+++ 2ndproject/script.js
@@ -0,0 +1,10 @@
+ function downloadResume() {
+    alert("Resume download feature coming soon!");
+}
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+function sendMessage() {
+    alert("Message sent successfully!");
+}
